d41303e74fcb8ceb6b886d4c1b991f53119af2b0
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-01-31 14:01:31 -0500

    Implementing cart store and adding products

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useCartStore from './store/store';
+
+function CurrentProducts() {
+  const [products, setProducts] = useState([]);
+  const addItem = useCartStore((state) => state.addItem);
+
+  const handleAddItem = (product) => {
+    const newItem = { id: product.id, name: product.title, price: product.price };
+    addItem(newItem);
+  };
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then(json => setProducts(json))
+  }, []);
+
+  return (
+    <div className='Current-Products'>
+      {products.map((product) => (
+        <div key={product.id} className='Product-Container'>
+          <img className='Product-Image' src={product.image} alt={`Product - ${product.title}`} />
+          <div className='Product-Information'>
+            <h3 className='Product-Title text-left'>{product.title}</h3>
+            <div className='text-left'><b>Description:</b> <span>{product.description}</span></div>
+          </div>
+          <div className='Product-Actions'>
+            <b>$ {product.price}</b>
+            <button onClick={() => handleAddItem(product)}>Add</button>
+          </div>
+        </div>
+      ))}
+      <div></div>
+    </div>
+  );
+}
+
+function CartBadge() {
+  const totalPrice = useCartStore((state) => state.totalPrice())
+
+  return (
+    <div className='Cart-Price'>Cart price: $ {totalPrice}</div>
+  );
+}
 
 function App() {
   return (
@@ -7,17 +52,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Shopping App example using Zustand
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <CartBadge />
+      <CurrentProducts />
     </div>
   );
 }
